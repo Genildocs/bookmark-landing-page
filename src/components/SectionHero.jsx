45b54cba207235993c442c4966a1001c9cdeeee9
genildocs <genildocs@gmail.com>
2024-01-25 01:44:22 -0300

    commit

--- src/components/SectionHero.jsx
+++ src/components/SectionHero.jsx
@@ -5,7 +5,7 @@ import Hero from "../assets/illustration-hero.svg";
 
 export default function SectionHero() {
   return (
-    <Grid container sx={{ paddingTop: "5rem" }}>
+    <Grid container gap={4} sx={{ paddingTop: "5rem" }}>
       <Grid item xs={12} md={6} sx={{ position: "relative" }}>
         <Box
           sx={{
@@ -25,8 +25,51 @@ export default function SectionHero() {
       </Grid>
       <Grid item xs={12} md={6}>
         <Box>
-          <Typography component={"h1"}>A Simple Bookmark Manager</Typography>
-          <Typography variant="body1" component={"p"}>
+          <Typography
+            component={"h1"}
+            sx={{
+              fontSize: {
+                xs: "1.8rem",
+                sm: "3rem",
+              },
+              fontFamily: "inherit",
+              fontWeight: 500,
+              color: "#242A45",
+              lineHeight: {
+                xs: "2.5rem",
+                sm: "3.2rem",
+              },
+              textAlign: {
+                xs: "center",
+                sm: "left",
+              },
+            }}
+          >
+            A Simple Bookmark Manager
+          </Typography>
+          <Typography
+            variant="body1"
+            component={"p"}
+            sx={{
+              fontSize: {
+                xs: "0.8rem",
+                sm: "1rem",
+              },
+              fontFamily: "inherit",
+              fontWeight: 400,
+              color: "#242A45",
+              lineHeight: {
+                xs: "1.5rem",
+                sm: "1.8rem",
+              },
+              textAlign: {
+                xs: "center",
+                sm: "left",
+              },
+              marginTop: { xs: "1rem" },
+              marginBottom: { xs: "1.5rem" },
+            }}
+          >
             A clean and simple interface to organize your favourite websites.
             Open a new browser tab and see your sites load instantly. Try it for
             free.
