4d6d21e59f846df186c8a98bec3b95d09fc92236
genildocs <genildocs@gmail.com>
2024-01-24 21:59:06 -0300

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Typography, List, ListItem } from '@mui/material';
-import Link from '@mui/material/Link';
-import Button from '@mui/material/Button';
-import Logo from '../assets/logo-bookmark.svg';
-import IconHamburger from '../assets/icon-hamburger.svg';
-import IconClose from '../assets/icon-close.svg';
+import React, { useState, useEffect } from "react";
+import { Box, Typography, List, ListItem } from "@mui/material";
+import Link from "@mui/material/Link";
+import Button from "@mui/material/Button";
+import Logo from "../assets/logo-bookmark.svg";
+import IconHamburger from "../assets/icon-hamburger.svg";
+import IconClose from "../assets/icon-close.svg";
 
 export default function Header({ mobile }) {
   const [isOpen, setIsOpen] = useState(false);
-  const listItem = ['Features', 'Pricing', 'Contact'];
+  const listItem = ["Features", "Pricing", "Contact"];
 
   const styleMenu = {
-    background: '#242A45',
-    position: 'absolute',
-    top: '0',
-    left: '0',
-    width: '100%',
-    height: '100vh',
-    display: 'flex',
-    alignItems: 'start',
-    justifyContent: 'space-between',
-    paddingTop: '3.75rem',
-    paddingLeft: '1.5rem',
-    paddingRight: '1.5rem',
+    background: "#242A45",
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+    alignItems: "start",
+    justifyContent: "space-between",
+    paddingTop: "3.75rem",
+    paddingLeft: "1.5rem",
+    paddingRight: "1.5rem",
     opacity: 0.95,
-    backdropFilter: 'blur(17px)',
-    transition: 'all 1s',
+    backdropFilter: "blur(17px)",
+    zIndex: 1000,
+    transition: "all 1s",
   };
 
   useEffect(() => {
     if (isOpen) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = "unset";
     }
   }, [isOpen]);
 
@@ -42,10 +43,10 @@ export default function Header({ mobile }) {
         isOpen
           ? styleMenu
           : {
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              paddingTop: '3.75rem',
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingTop: "3.75rem",
             }
       }
     >
@@ -55,48 +56,48 @@ export default function Header({ mobile }) {
       <Box>
         <List
           sx={{
-            display: 'flex',
-            alignItems: 'center',
+            display: "flex",
+            alignItems: "center",
             flexDirection: {
-              xs: 'column',
-              sm: 'row',
+              xs: "column",
+              sm: "row",
             },
             position: {
-              xs: 'absolute',
-              sm: 'static',
+              xs: "absolute",
+              sm: "static",
             },
-            top: isOpen ? '30%' : '0',
-            left: '50%',
+            top: isOpen ? "30%" : "0",
+            left: "50%",
             transform: {
-              xs: 'translate(-50%, -50%)',
-              sm: 'translate(0, 0)',
+              xs: "translate(-50%, -50%)",
+              sm: "translate(0, 0)",
             },
             opacity: {
               xs: isOpen ? 1 : 0,
               sm: 1,
             },
             visibility: {
-              xs: isOpen ? 'visible' : 'hidden',
-              sm: 'visible',
+              xs: isOpen ? "visible" : "hidden",
+              sm: "visible",
             },
             transition: {
-              xs: isOpen ? 'all 1.5s' : 'none',
-              sm: 'none',
+              xs: isOpen ? "all 1.5s" : "none",
+              sm: "none",
             },
           }}
         >
           {listItem.map((item) => (
-            <ListItem style={{ width: 'fit-content' }} key={item}>
+            <ListItem style={{ width: "fit-content" }} key={item}>
               <Link
                 href="#"
                 underline="none"
                 textTransform="uppercase"
                 sx={{
                   marginBottom: {
-                    xs: '1.5rem',
-                    sm: '0',
+                    xs: "1.5rem",
+                    sm: "0",
                   },
-                  color: { xs: 'white', sm: 'black' },
+                  color: { xs: "white", sm: "black" },
                 }}
               >
                 {item}
@@ -110,27 +111,27 @@ export default function Header({ mobile }) {
               pl: 5,
               pr: 5,
               background: {
-                xs: 'transparent',
-                sm: '#FA5959',
+                xs: "transparent",
+                sm: "#FA5959",
               },
-              color: 'white',
+              color: "white",
               border: {
-                xs: '2px solid white',
-                sm: '2px solid #FA5959',
+                xs: "2px solid white",
+                sm: "2px solid #FA5959",
               },
-              fontFamily: 'inherit',
-              letterSpacing: '1.5px',
+              fontFamily: "inherit",
+              letterSpacing: "1.5px",
               transition: {
-                xs: 'none',
-                sm: 'background 0.5s ease-in-out, color 0.3s ease-in-out, border 0.5s ease-in-out',
+                xs: "none",
+                sm: "background 0.5s ease-in-out, color 0.3s ease-in-out, border 0.5s ease-in-out",
               },
 
-              '&:hover': {
-                xs: { border: '2px solid white' },
+              "&:hover": {
+                xs: { border: "2px solid white" },
                 sm: {
-                  background: 'white',
-                  color: '#FA5959',
-                  border: '2px solid #FA5959',
+                  background: "white",
+                  color: "#FA5959",
+                  border: "2px solid #FA5959",
                 },
               },
             }}
@@ -141,8 +142,8 @@ export default function Header({ mobile }) {
 
         <Button
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            cursor: 'pointer',
+            display: { xs: "block", sm: "none" },
+            cursor: "pointer",
           }}
           onClick={() => setIsOpen(!isOpen)}
         >
--- src/components/SectionHero.jsx
+++ src/components/SectionHero.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import Grid from '@mui/material/Grid';
-import Hero from '../assets/illustration-hero.svg';
-import Rectangle from '../assets/Rectangle.png';
+import React from "react";
+import { Box, Typography, Button } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Hero from "../assets/illustration-hero.svg";
 
 export default function SectionHero() {
   return (
-    <Grid container sx={{ paddingTop: '5rem' }}>
-      <Grid item xs={12} md={6}>
-        <Box sx={{}}>
-          <img src={Rectangle} alt="Rectangle" />
+    <Grid container sx={{ paddingTop: "5rem" }}>
+      <Grid item xs={12} md={6} sx={{ position: "relative" }}>
+        <Box
+          sx={{
+            width: "36rem",
+            height: "12.5rem",
+            borderRadius: "16rem",
+            backgroundColor: "#5267DF",
+            position: "absolute",
+            left: "10%",
+            top: "15%",
+            zIndex: -1,
+          }}
+        ></Box>
+        <Box>
+          <img src={Hero} alt="Hero" style={{ width: "100%", zIndex: 1 }} />
         </Box>
-        <img src={Hero} alt="Hero" style={{ width: '100%' }} />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Box>
+          <Typography component={"h1"}>A Simple Bookmark Manager</Typography>
+          <Typography variant="body1" component={"p"}>
+            A clean and simple interface to organize your favourite websites.
+            Open a new browser tab and see your sites load instantly. Try it for
+            free.
+          </Typography>
+          <Grid container spacing={2} wrap="nowrap">
+            <Grid item>
+              <Button variant="contained">Get it on Chrome</Button>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined">Get it on Firefox</Button>
+            </Grid>
+          </Grid>
+        </Box>
       </Grid>
     </Grid>
   );
